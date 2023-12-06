8c7f29c849b3ad2a266b6947e45f2cc1eb7ca687
Oğuzhan <kayaoguzhan@windowslive.com>
2023-12-06 21:43:47 +0300

    completed

---
+++ GlobalErrorHandling/Middlewares/GlobalExceptionHandlingMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Text.Json;
+
+namespace GlobalErrorHandling.Middlewares
+{
+    public class GlobalExceptionHandlingMiddleware : IMiddleware
+    {
+
+        private readonly ILogger<GlobalExceptionHandlingMiddleware> _logger;
+
+        public GlobalExceptionHandlingMiddleware(ILogger<GlobalExceptionHandlingMiddleware> logger) 
+        {
+            _logger = logger;
+                
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, e.Message);
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                ProblemDetails problem = new()
+                {
+                    Status = (int)HttpStatusCode.InternalServerError,
+                    Type = "Server error",
+                    Title = "Server error",
+                    Detail = "An internal server has occured"
+                };
+
+                string json = JsonSerializer.Serialize(problem);
+
+                await context.Response.WriteAsync(json);
+
+                context.Response.ContentType = "application/json";
+            }
+        }
+    }
+}
